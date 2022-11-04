1f526b60d2524fa710a06a5454b46ae6d7a3a963
Aeneas Schwaiger <schwaiger@max-eyth-schule.de>
2022-11-04 12:37:34 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Taschenrechner.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Zutaten:
+	int zahl1 = 0;
+	double zahl2 = 0.0;
+	double erg = 0;
+	char rechenzeichen = '_';
+
+	// Zubereitung (Algorythmus)
+	cout << "Bitte geben sie erste Zahl ein: " << endl;
+	cin >> zahl1;
+	cout << "Bitte geben sie zweite Zahl ein: " << endl;
+	cin >> zahl2;
+
+	cout << "Rechenzeichen eigeben: " << endl;
+	cin >> rechenzeichen;
+
+	cout << "Zahl1: " << zahl1 << endl;
+	cout << "Zahl2: " << zahl2 << endl;
+	cout << "Rechenzeichen: " << rechenzeichen << endl;
+
+	switch (rechenzeichen)
+	{
+	case '+':  
+		erg = zahl1 + zahl2;
+		cout << "Ergebnis: " << erg << endl;
+		break;
+		
+
+	case '-': 
+		erg = zahl1 - zahl2;
+		cout << "Ergebnis: " << erg << endl;
+		break;
+
+	case '*':
+		erg = zahl1 * zahl2;
+		cout << "Ergebnis: " << erg << endl;
+		break;
+
+	case '/':
+		erg = zahl1 / zahl2;
+		cout << "Ergebnis: " << erg << endl;
+		break;
+
+	default:
+		cout << "Du bist zu ..." << endl;
+		break;
+
+	}
+	cout << "Ende" << endl;
+ 
+	
+	return 0;
+}
